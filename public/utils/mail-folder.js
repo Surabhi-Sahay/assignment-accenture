852d321155073270d83d3703d97910751d4ff7b8
Surabhi-Sahay <surabhisahay307@gmail.com>
2020-09-26 22:51:50 +0530

    folders and json updated

---
+++ public/utils/mail-folder.js
@@ -0,0 +1,27 @@
+export const folders =[
+	{
+		"id": "1",
+		"name": "Inbox"
+	},
+	{
+		"id": "2",
+		"name": "Sent"
+    },
+    {
+		"id": "3",
+		"name": "Spam"
+    },
+    {
+		"id": "4",
+		"name": "Drafts"
+    },
+    {
+		"id": "5",
+		"name": "Delete"
+    },
+    {
+		"id": "6",
+		"name": "Archieve"
+    }
+    
+];
---
+++ src/components/folder-component/folder-component.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import './folder-component.css';
+
+export default class FolderComponent extends Component{
+    render (){
+        return (
+           <div></div>
+        )
+    }
+}
